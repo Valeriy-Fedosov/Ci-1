90131b214df54d411c6ff7052f2a7443c26b2db2
Valeriy-Fedosov <fedossov_egmedu@mail.ru>
2025-03-03 19:59:55 +0300

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a{5}, b{6};
+    cout << "Imput two numbers" << endl;
+    cout << "1 number" << endl;
+    cout << "a + b = "<< a + b << endl;
+    return 0;
+}
